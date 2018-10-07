7fe2273c4d4046c2d6eb0d0af400163adb55357f
Jonathan Shan <johnny.shan@gmail.com>
2018-10-07 16:28:36 -0700

    finished lab2

--- stack_linked.py
+++ stack_linked.py
@@ -4,39 +4,53 @@ class Node:
         self.next = None
 
 class Stack:
-    '''Implements an efficient last-in first-out Abstract Data Type using a Linked List'''
+    """Implements an efficient last-in first-out Abstract Data Type using a Linked List"""
 
     def __init__(self, capacity):
-        '''Creates and empty stack with a capacity'''
+        """Creates and empty stack with a capacity"""
         self.capacity = capacity
         self.head = None
-        self.num_items = 0
+        self.count = 0
 
     def is_empty(self):
-        '''Returns True if the stack is empty, and False otherwise
-           MUST have O(1) performance'''
+        """Returns True if the stack is empty, and False otherwise"""
+        if self.head == None:
+            return True
+        return False
 
     def is_full(self):
-        '''Returns True if the stack is full, and False otherwise
-           MUST have O(1) performance'''
+        """Returns True if the stack is full, and False otherwise"""
+        if self.count == self.capacity:
+            return True
+        return False
 
     def push(self, item):
-        '''If stack is not full, pushes item on stack. 
-           If stack is full when push is attempted, raises IndexError
-           MUST have O(1) performance'''
+        """If stack is not full, pushes item on stack. 
+        If stack is full when push is attempted, raises IndexError"""
+        if self.is_full():
+            raise IndexError
+        newNode = Node(item)
+        newNode.next = self.head
+        self.head = newNode
+        self.count += 1
 
     def pop(self): 
-        '''If stack is not empty, pops item from stack and returns item.
-           If stack is empty when pop is attempted, raises IndexError
-           MUST have O(1) performance'''
+        """If stack is not empty, pops item from stack and returns item.
+        If stack is empty when pop is attempted, raises IndexError"""
+        if self.is_empty():
+            raise IndexError
+        returnvalue = self.head.data
+        self.head = self.head.next
+        self.count -= 1
+        return returnvalue
 
     def peek(self):
-        '''If stack is not empty, returns next item to be popped (but does not pop the item)
-           If stack is empty, raises IndexError
-           MUST have O(1) performance'''
-
+        """If stack is not empty, returns next item to be popped (but does not pop the item)
+        If stack is empty, raises IndexError"""
+        if self.is_empty():
+            raise IndexError
+        return self.head.data
     def size(self):
-        '''Returns the number of elements currently in the stack, not the capacity
-           MUST have O(1) performance'''
-
+        """Returns the number of elements currently in the stack, not the capacity"""
+        return self.count
  
--- stack_tests.py
+++ stack_tests.py
@@ -3,15 +3,59 @@ import unittest
 # Use the imports below to test either your array-based stack
 # or your link-based version
 from stack_array import Stack
-# from stack_linked import Stack
+from stack_linked import Stack
 
 class TestLab2(unittest.TestCase):
     def test_simple(self):
         stack = Stack(5)
+        self.assertRaises(IndexError, stack.pop)
         stack.push(0)
         self.assertFalse(stack.is_empty())
         self.assertFalse(stack.is_full())
         self.assertEqual(stack.size(),1)
+
+    def test_simple_1(self):
+        stack = Stack(3)
+        self.assertRaises(IndexError, stack.peek)
+        stack.push(0)
+        stack.push(1)
+        stack.push(2)
+        self.assertRaises(IndexError, stack.push, 3)
+        self.assertTrue(stack.is_full())
+        self.assertEqual(stack.size(),3)
+        stack.pop()
+        stack.pop()
+        self.assertEqual(stack.pop(), 0)
+
+
+    def test_linked(self):
+    	stack = Stack(10)
+    	stack.push('a')
+    	stack.pop()
+    	self.assertEqual(stack.size(),0)
+    	self.assertTrue(stack.is_empty())
+    	stack.push('a')
+    	stack.push('b')
+    	stack.push('c')
+    	stack.push('d')
+    	stack.push('e')
+    	stack.push('f')
+    	stack.push('g')
+    	stack.push('h')
+    	stack.push('i')
+    	stack.push('j')
+    	self.assertTrue(stack.is_full())
+    	self.assertRaises(IndexError, stack.push,'k')
+    	stack.pop()
+    	self.assertEqual(stack.peek(), 'i')
+    	self.assertEqual(stack.size(), 9)
+
+    def test_linked_1(self):
+    	stack = Stack(4)
+    	self.assertRaises(IndexError, stack.peek)
+    	self.assertRaises(IndexError, stack.pop)
+    	self.assertTrue(stack.is_empty())
+    	self.assertFalse(stack.is_full())    	
 
 if __name__ == '__main__': 
     unittest.main()
